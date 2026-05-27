2f4cc060f2818a8baf90a09c82916be1ffa27768
kseweryn <kseweryn@gusite.us.edu.pl>
2026-05-27 09:12:10 +0200

    drugi commit (klienci2)

--- clients.py
+++ clients.py
@@ -4,13 +4,17 @@ import services
 class Customer
     def __init__(self, name, surname, birth_date, email):
         self.name = name
+        self.surname = surname
+        self.birth_date = birth_date
+        self.email = email
+
 
 def order_transport(self, service):
-    services.Transport()
+    services.order_transport()
     print(self.name)
     print(self.surname)
     print(self.birth_date)
     print(self.email)
 
 
-
+dd
