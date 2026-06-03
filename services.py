a5325ecc8d8b31731e58dbe1a6abf6477b55add0
ksewern011 <kaseweryn06@gmail.com>
2026-06-03 09:08:22 +0200

    Add TaxiService and ScooterService classes

--- services.py
+++ services.py
@@ -31,3 +31,21 @@ class BikeService(TransportService):
 
     def transport_name(self):
         return "Bike"
+
+
+
+class TaxiService(TransportService):
+    def create_transport(self):
+        return Taxi()
+
+    def transport_name(self):
+        return "Taxi"
+
+
+
+class ScooterService(TransportService):
+    def create_transport(self):
+        return Scooter()
+
+    def transport_name(self):
+        return "Scooter"
