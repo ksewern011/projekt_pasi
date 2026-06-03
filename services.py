8f7fa8bf8e38d47148d2d984790d9a77d9ca2b21
Mateusz <mateusz.sakiewicz@gmail.com>
2026-06-03 08:20:04 +0200

    ostateczny commit przed branchami

--- services.py
+++ services.py
@@ -2,9 +2,6 @@ from transport import *
 from abc import ABC, abstractmethod
 
 class TransportService(ABC):
-    def __init__(self):
-        self.available = True
-
 
     @abstractmethod
     def create_transport(self):
@@ -15,15 +12,12 @@ class TransportService(ABC):
         pass
 
     def order_transport(self):
-        if self.available:
-            transport = self.create_transport()
-            print(f"pomyślnie zamówiono: {self.transport_name()}")
-            print(f"typ pojazdu: {transport.vehicle_type()}")
-            print(f"przewidywany czas przyjazdu: {transport.arrival_time()}")
-            print(f"przewidywany czas podróży: {transport.travel_time()}")
-            self.available = False
-        else:
-            print(f"usługa {self.transport_name()} jest w tej chwili niedostępna")
+        transport = self.create_transport()
+        print(f"pomyślnie zamówiono: {self.transport_name()}")
+        print(f"typ pojazdu: {transport.vehicle_type()}")
+        print(f"przewidywany czas przyjazdu: {transport.arrival_time()}")
+        print(f"przewidywany czas podróży: {transport.travel_time()}")
+
 
 
 class BikeService(TransportService):
