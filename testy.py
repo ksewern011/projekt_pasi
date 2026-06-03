9425962779dddb15e7d6b1e6d74d19eb050749f9
Mateusz <mateusz.sakiewicz@gmail.com>
2026-06-03 09:33:33 +0200

    uzuplenienie testow

--- testy.py
+++ testy.py
@@ -1,3 +1,21 @@
 import pytest
+from services import *
+from transport import *
+
+def test_tworzenie_transportu():
+    service = BikeService()
+    pojazd = service.create_transport()
+    assert isinstance(pojazd, Bike)
+def test_dzialanie_metod():
+    service = BikeService()
+    pojazd = service.create_transport()
+    assert pojazd.vehicle_type() == "Bike"
+    assert pojazd.arrival_time() == "5 minutes"
+def test_mechanizm_dostepnosci():
+    service = BikeService()
+    service.order_transport()
+    assert service.available == False
+
+
 
 
